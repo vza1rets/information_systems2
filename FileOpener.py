dfc1ec8617f2444842ea88227287ec883fc543cf
callme1ngin <antones270@gmail.com>
2025-09-10 17:47:09 +0700

    File reader splitted, added main def

--- FileOpener.py
+++ FileOpener.py
@@ -1,10 +1,11 @@
-print('File or string?')
-typee = input()
-strings = []
-if typee.lower() == 'file':
-    path = input('Введите путь к файлу: ')
-    with open(f'{path}', encoding='utf-8') as f:
-        strings = f.readlines()
-        strings = [line.rstrip() for line in strings]
-elif typee.lower() == 'string':
-    strings.append('"Дизель"             2025.04.27 66.43         "Газпром Нефть" 10000')
+
+    print('File or string?')
+    typee = input()
+    strings = []
+    if typee.lower() == 'file':
+        path = input('Введите путь к файлу: ')
+        with open(f'{path}', encoding='utf-8') as f:
+            strings = f.readlines()
+            strings = [line.rstrip() for line in strings]
+    elif typee.lower() == 'string':
+        strings.append('"Дизель"             2025.04.27 66.43         "Газпром Нефть" 10000')
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
 import shlex
-from FileOpener import strings
+
 
 class ExtendedOilPrice:
     def __init__(self, oil_type: str, datee : date, price: float, supplier : str, volume : float):
@@ -10,20 +10,55 @@ class ExtendedOilPrice:
         self.supplier = supplier
         self.volume = volume
     def __str__(self):
-        return f"Тип топлива: {self.oil_type}, Дата: {self.datee.strftime('%Y.%m.%d')}, Цена: {self.price}, Поставщик: {self.supplier}, Объем: {self.volume}"
+        return (f"Тип топлива: {self.oil_type}, Дата: {self.datee.strftime('%Y.%m.%d')}, Цена: "
+                f"{self.price}, Поставщик: {self.supplier}, Объем: {self.volume}")
 
-input_str = strings
+def read_lines_from_file(path):
+    with open(path, encoding="utf-8") as f:
+        return [line.rstrip() for line in f]
 
-def trytoparse_extendedoil(string) -> ExtendedOilPrice:
+def read_lines_interactive():
+    print("Вводите строки; пустая строка завершит ввод.")
+    lines = []
+    while True:
+        s = input().strip()
+        if not s:
+            break
+        lines.append(s)
+    return lines
+
+def trytoparse_extendedoil(strings) -> ExtendedOilPrice:
     for string in strings:
         parts = shlex.split(string)
         price = float(parts[2])
         oil_type = parts[0].strip('"')
         datee = (datetime.strptime(str(parts[1]), "%Y.%m.%d")).date()
         supplier = parts[3]
-        volume = parts[4]
+        volume = float(parts[4])
         yield ExtendedOilPrice(oil_type, datee, price, supplier, volume)
 
-for string in trytoparse_extendedoil(input_str):
-    print(string)
+def find_maxprice(strings):
+    maxprice = 0
+    for string in strings:
+        parts = shlex.split(string)
+        if float(parts[2]) > maxprice:
+            maxprice = float(parts[2])
+    print(f'Максимальная цена: {maxprice}')
+
+def main():
+    typee = input('File or string? ').lower()
+    if typee == 'file':
+        strings = read_lines_from_file((input('Путь к файлу: ')))
+    elif typee == 'string':
+        strings = read_lines_interactive()
+    for obj in trytoparse_extendedoil(strings):
+        print(obj)
+    if len(strings) >= 2:
+        find_maxprice(strings)
+
+
+if __name__ == "__main__":
+    main()
+
+
 
